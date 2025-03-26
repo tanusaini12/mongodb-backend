978feb449da3956be8f04706c3ce5ff2d0160e68
tanusaini12 <sainitanu2003@gmail.com>
2025-03-26 14:24:56 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,96 +1,83 @@
-//Tanu Saini, 0004831
+//Tanu Saini, SID-4831
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const db = require('./db/database');
 
 const app = express();
 const port = 3000;
-const connectString = 'mongodb+srv://sainitanu2003:Saini12345@cluster0.yao1e.mongodb.net/FCollege'
-
 
 app.use(bodyParser.json());
-app.use(cors()); //Enable cors
+app.use(cors());
 
-mongoose
-    .connect(connectString, {})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Error Connecting to MongoDB:' , err));
-
-    const userSchema = new mongoose.Schema({
-        name: {type:String, required:true},
-        email: {type:String, required:true}
-    });
-
-    const User = mongoose.model('User,', userSchema);
-
-    //Routes
-
-    //Create a new user
-    app.post('/users', async(req, res) => {
-        try {
-            const {name, email} = req.body
-            const user = new User({name, email});
-            await user.save();
-            res.status(201).send(user);
-        } catch (error) {
-            res.status(500).send(error.message);
+//create a new user
+app.post('/users', (req, res) => {
+const {name, email} = req.body;
+    const query = 'INSERT INTO users (name, email) VALUES (?,?)'
+    db.run(query, [name, email], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
         }
+        res.status(201).send({id: this.LastID});
     });
+});
 
-   //read all users
-   app.get('/users', async (req,res)=>{
-    try{
-        const users = await User.find();
-        res.send(users);
+//Read all users
+app.get('/users', (req, res) => {
+    const query = 'SELECT * from users';
+    db.all(query,[], (err, rows) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        } 
+        res.status(200).json(rows);
+       });
+});
+
+// Read a single user by id
+app.get('/users/:id', (req,res) => {
+    const query = "SELECT * FROM users WHERE id = ?";
+    const {id} = req.params;
+    db.get(query, [id], (err, row) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        if (!row) {
+            return res.status(404).send('user not found');
+        }
+        res.status(200).json(row);
+    });
+});
+
+//update a user by id
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const {name, email} = req.body;
+    const query ='UPDATE users SET name = ?, email = ? WHERE id = ?';
+    db.run(query, [name, email, id], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        if (this.changes === 0) {
+            return res.status(404).send('User Not Found');
+        }
+        res.status(200).send('user update successfully');
+    });
+});
+
+//delete a user by id
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const query = "DELETE FROM users WHERE id = ?";
+    db.run(query,[id], function(err) {
+        if (err) {
+        return res.status(500).send(err.message);
     }
-    catch(error){
-        res.status(500).send(error.message);
+    if (this.changes === 0) {
+        return res.status(404).send('User Not Found');
     }
- });
-
- //read a single user
- app.get('/users/:id', async (req,res)=>{
-    try{
-        const {id} = req.params;
-        const user = await User.findById(id);
-        if(!user) {
-           return res.status(404).send('User not found');
-        }
-        res.status(200).send(user);
-     }catch (error){
-        res.status(500).send(error.message);
-     }
-  });
-  //update user by id
-  app.put('/users/:id', async (req, res) => {
-    try{
-        const { id } = req.params;
-        const {name, email } = req.body;
-        const user = await User.findByIdAndUpdate(id, {name, email}, {new:true});
-        if(!user) {
-            return res.status(404).send('User not found');
-        }
-        res.status(200).send('User Updated Succesfully');        
-        } catch(error) {
-                res.status(500).send(error.message);
-            }
-               });
-
-               //Delete user by id
-               app.delete('/users/:id', async (req, res) => {
-                try{
-                    const{ id } = req.params;
-                    const user = await User.findByIdAndDelete(id);
-                if(!user) {
-                    return res.status(404).send('User not found');
-                }          
-            res.status(200).send('User deleted successfully');
-              } catch (error) {
-                res.status(500).send(error.message);
-              }
-               });
-    //Start the server
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
+    res.status(200).send('user deleted successfully');
     });
+});
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
